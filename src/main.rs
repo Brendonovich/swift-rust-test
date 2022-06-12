3d40135ec2089f98cf1176c3e40ac01bf016083b
Brendan Allan <brendonovich@outlook.com>
2022-06-12 17:08:02 +0800

    woah it's working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use swift_rs::SRData;
+use serde_json::Value;
+use swift_rs::*;
+
+swift_fn!(test_struct() -> SRData);
 
 fn main() {
-    unsafe {
-        objc_autoreleasePoolPush();
-        // Commenting out the below line will show everything is working as expected
-        test_struct();
-        objc_autoreleasePoolPop();
-    };
+    let pool = unsafe { objc_autoreleasePoolPush() };
+
+    let data = test_struct();
+    let json = serde_json::from_slice::<Value>(&data).unwrap();
+    println!("{}", serde_json::to_string_pretty(&json).unwrap());
+
+    unsafe { objc_autoreleasePoolPop(pool) };
 }
 
 extern "C" {
-    fn objc_autoreleasePoolPush();
-    #[allow(dead_code)]
-    fn test_struct() -> SRData;
-    fn objc_autoreleasePoolPop();
+    fn objc_autoreleasePoolPush() -> *mut std::ffi::c_void;
+    fn objc_autoreleasePoolPop(pool: *mut std::ffi::c_void);
 }
